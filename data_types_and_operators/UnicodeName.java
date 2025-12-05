1a117b007bd058416060c7e997406236edc4acd9
Sergey <167028015+sirgay1@users.noreply.github.com>
2025-12-05 20:34:32 +0300

    Add files via upload

---
+++ data_types_and_operators/UnicodeName.java
@@ -0,0 +1,11 @@
+
+public class UnicodeName {
+    public static void main(String[] args) {
+        System.out.print("\u0421");   // С
+        System.out.print("\u0435");   // е
+        System.out.print("\u0440");   // р
+        System.out.print("\u0433");   // г
+        System.out.print("\u0435");   // е
+        System.out.println("\u0439"); // й
+    }
+}
